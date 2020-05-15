79d11b7319fe7cfbdbfdd3b1c3880e2133f0f69c
Diego Garza <dgarza8988@gmail.com>
2020-05-15 12:51:32 +0200

    Added swagger docs

--- src/services/talent/controllers/talents.ts
+++ src/services/talent/controllers/talents.ts
@@ -5,6 +5,66 @@ import { ControllerCRUD } from "platform-api";
 import { Controller, Get, Post, Put, Delete, Auth } from "platform-api";
 import { applyOnFields } from "platform-api";
 
+/**
+ * @swagger
+ *
+ * components:
+ *   schemas:
+ *     TalentNew:
+ *       type: object
+ *       required:
+ *         - name
+ *         - headline
+ *         - bio_highlights
+ *         - bio_details
+ *       properties:
+ *         enabled:
+ *           type: boolean
+ *         name:
+ *           type: string
+ *         headline:
+ *           type: string
+ *         bio_highlights:
+ *           type: string
+ *         bio_details:
+ *           type: string
+ *         social_accounts:
+ *           type: object
+ *           additionalProperties:
+ *             type: string
+ *         metadata:
+ *           type: object
+ *         reviews:
+ *           type: array
+ *           items:
+ *             type: object
+ *         categories:
+ *           type: array
+ *           items:
+ *             type: string
+ *         topics:
+ *           type: array
+ *           items:
+ *             type: string
+ *         agents:
+ *           type: array
+ *           readOnly: true
+ *           items:
+ *             type: string
+ *
+ *     Talent:
+ *       allOf:
+ *         - $ref: '#/components/schemas/TalentNew'
+ *         - type: object
+ *           required:
+ *             - id
+ *           properties:
+ *             id:
+ *               type: string
+ *               readOnly: true
+ *
+ */
+
 @Controller("/v1/talents")
 class TalentsController extends ControllerCRUD {
   public entityName = "talent";
@@ -16,6 +76,32 @@ class TalentsController extends ControllerCRUD {
     super.start();
   }
 
+  /**
+   * @swagger
+   *
+   * /v1/talents/:
+   *   get:
+   *     summary: Get a list of talents
+   *     tags:
+   *       - Talent
+   *     parameters:
+   *       - $ref: '#/components/parameters/offsetParam'
+   *       - $ref: '#/components/parameters/pageParam'
+   *       - $ref: '#/components/parameters/limitParam'
+   *       - $ref: '#/components/parameters/orderParam'
+   *       - $ref: '#/components/parameters/fieldsParam'
+   *       - $ref: '#/components/parameters/whereParam'
+   *       - $ref: '#/components/parameters/whereSomeParam'
+   *     responses:
+   *       '200':
+   *         description: OK
+   *         content:
+   *           application/json:
+   *             schema:
+   *               type: array
+   *               items:
+   *                 $ref: '#/components/schemas/Talent'
+   */
   @Get("/")
   // @Auth({ roles: ["realm:admin"] })
   protected async find(
@@ -32,6 +118,26 @@ class TalentsController extends ControllerCRUD {
     return super.find(req, res, whereValues);
   }
 
+  /**
+   * @swagger
+   *
+   * /v1/talents/{id}:
+   *   get:
+   *     summary: Get a talent entry
+   *     security:
+   *       - BearerAuth: []
+   *     tags:
+   *       - Talent
+   *     parameters:
+   *       - $ref: '#/components/parameters/idParam'
+   *     responses:
+   *       '200':
+   *         description: OK
+   *         content:
+   *           application/json:
+   *             schema:
+   *               $ref: '#/components/schemas/Talent'
+   */
   @Get("/:id")
   @Auth({ permissions: ["talent:${id}?view"] })
   protected async get(
@@ -41,6 +147,45 @@ class TalentsController extends ControllerCRUD {
     return super.get(req, res);
   }
 
+  /**
+   * @swagger
+   *
+   * /v1/talents/:
+   *   post:
+   *     summary: Create a new talent entry
+   *     security:
+   *       - BearerAuth: []
+   *     tags:
+   *       - Talent
+   *     requestBody:
+   *       required: true
+   *       content:
+   *         application/json:
+   *           schema:
+   *             $ref: '#/components/schemas/TalentNew'
+   *           example:
+   *             name: Alessandro Iob
+   *             headline: "He is somebody from Italy"
+   *             bio_highlights: "Wrote some software"
+   *             bio_details: "Sometimes that software worked!"
+   *             social_accounts:
+   *               github: "https://github.com/alexiob"
+   *             metadata: {}
+   *             reviews: []
+   *             notes: "Can he really write software?"
+   *             categories:
+   *               - software
+   *               - strange
+   *             topics:
+   *               - random
+   *     responses:
+   *       '200':
+   *         description: OK
+   *         content:
+   *           application/json:
+   *             schema:
+   *               $ref: '#/components/schemas/Talent'
+   */
   @Post("/")
   @Auth({ roles: ["realm:talent_manager"] })
   protected async post(
@@ -67,6 +212,47 @@ class TalentsController extends ControllerCRUD {
     return this.normalizePayload(payload);
   }
 
+  /**
+   * @swagger
+   *
+   * /v1/talents/{id}:
+   *   put:
+   *     summary: Update a talent entry
+   *     security:
+   *       - BearerAuth: []
+   *     tags:
+   *       - Talent
+   *     parameters:
+   *       - $ref: '#/components/parameters/idParam'
+   *     requestBody:
+   *       required: true
+   *       content:
+   *         application/json:
+   *           schema:
+   *             $ref: '#/components/schemas/TalentNew'
+   *           example:
+   *             name: Alessandro Iob
+   *             headline: "He is somebody from Italy"
+   *             bio_highlights: "Wrote some software"
+   *             bio_details: "Sometimes that software worked!"
+   *             social_accounts:
+   *               github: "https://github.com/alexiob"
+   *             metadata: {}
+   *             reviews: []
+   *             notes: "Can he really write software?"
+   *             categories:
+   *               - software
+   *               - strange
+   *             topics:
+   *               - random
+   *     responses:
+   *       '200':
+   *         description: OK
+   *         content:
+   *           application/json:
+   *             schema:
+   *               $ref: '#/components/schemas/Talent'
+   */
   @Put("/:id")
   @Auth({ permissions: ["talent:${id}?write"] })
   protected async put(
@@ -87,6 +273,26 @@ class TalentsController extends ControllerCRUD {
     return this.normalizePayload(payload);
   }
 
+  /**
+   * @swagger
+   *
+   * /v1/talents/{id}:
+   *   delete:
+   *     summary: Delete a talent entry
+   *     security:
+   *       - BearerAuth: []
+   *     tags:
+   *       - Talent
+   *     parameters:
+   *       - $ref: '#/components/parameters/idParam'
+   *     responses:
+   *       '200':
+   *         description: OK
+   *         content:
+   *           application/json:
+   *             schema:
+   *               $ref: '#/components/schemas/Talent'
+   */
   @Delete("/:id")
   @Auth({ permissions: ["talent:${id}?write"] })
   protected async delete(
@@ -96,6 +302,34 @@ class TalentsController extends ControllerCRUD {
     return super.delete(req, res);
   }
 
+  /**
+   * @swagger
+   *
+   * /v1/talents/{id}/agent:
+   *   put:
+   *     summary: Add an agent user to a talent entry
+   *     security:
+   *       - BearerAuth: []
+   *     tags:
+   *       - Talent
+   *     parameters:
+   *       - $ref: '#/components/parameters/idParam'
+   *     requestBody:
+   *       required: true
+   *       content:
+   *         application/json:
+   *           schema:
+   *             type: object
+   *             properties:
+   *               agent_id:
+   *                 type: string
+   *                 required: true
+   *           example:
+   *             agent_id: "alessandro_iob"
+   *     responses:
+   *       '200':
+   *         description: OK
+   */
   @Put("/:id/agent")
   @Auth({ permissions: ["talent:${id}?write"] })
   protected async addAgent(
@@ -122,6 +356,33 @@ class TalentsController extends ControllerCRUD {
     }
   }
 
+  /**
+   * @swagger
+   *
+   * /v1/talents/{id}/agent:
+   *   delete:
+   *     summary: Remove an agent user to a talent entry
+   *     security:
+   *       - BearerAuth: []
+   *     tags:
+   *       - Talent
+   *     parameters:
+   *       - $ref: '#/components/parameters/idParam'
+   *     requestBody:
+   *       required: true
+   *       content:
+   *         application/json:
+   *           schema:
+   *             type: object
+   *             properties:
+   *               agent_id:
+   *                 type: string
+   *           example:
+   *             agent_id: "alessandro_iob"
+   *     responses:
+   *       '200':
+   *         description: OK
+   */
   @Delete("/:id/agent")
   @Auth({ permissions: ["talent:${id}?write"] })
   protected async removeAgent(

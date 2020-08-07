f623ba91ba920029ec83c2ce5835992b9e420632
Diego Garza <dgarza8988@gmail.com>
2020-08-07 09:49:36 +0200

    fixed email address

--- src/services/todo/controllers/todos.ts
+++ src/services/todo/controllers/todos.ts
@@ -227,7 +227,7 @@ class TodosController extends ControllerCRUD {
 
   protected async notifyUser(userId: string, todo: any) {
     const options: any = {
-      fromAddress: "mvp-app-bot@nything.cluster1.endvr-digital-dev.com",
+      fromAddress: "mvp-app-bot@mvp-app.cluster1.endvr-digital-dev.com",
     };
 
     const targetMessages = [
